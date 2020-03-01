33fc39e709eaaca21f0779788e2274d32e7aeac7
Abdelrahman <abdelrahman@brown.edu>
2020-03-01 18:00:28 -0500

    update metrics list

--- src/views/TSMC65LP.vue
+++ src/views/TSMC65LP.vue
@@ -11,7 +11,7 @@
             </template>
             <template slot="thead">
               <vs-th>timestamp</vs-th>
-              <vs-th :key="build" v-for="(x, build) in gcdBuilds">{{gcdBuilds[build].timestamp}}</vs-th>
+              <vs-th :key="build" v-for="(x, build) in gcdBuilds">{{gcdBuilds[build].generate_date}}</vs-th>
             </template>
 
             <template slot-scope="{data}">
@@ -41,17 +41,6 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 export default {
-  data: () => ({
-    colorx: "dark",
-    gcdBuilds: [],
-    metrics: [
-      { name: "commit" },
-      { name: "uuid" },
-      { name: "openroad_version" },
-      { name: "yosys_version" },
-      { name: "yosys_cell_count" }
-    ]
-  }),
   created: function() {
     this.loadMetrics();
   },
@@ -62,7 +51,7 @@ export default {
 
       gcdMetrics
         .get()
-        .then((doc) => {
+        .then(doc => {
           if (doc.exists) {
             this.gcdBuilds = doc.data().builds;
           } else {
@@ -73,7 +62,133 @@ export default {
           console.log("Error getting metrics:", error);
         });
     }
-  }
+  },
+  data: () => ({
+    colorx: "dark",
+    gcdBuilds: [],
+    metrics: [
+      { name: "comment" },
+      { name: "cts_cpu" },
+      { name: "cts_mem" },
+      { name: "cts_time" },
+      { name: "design" },
+      { name: "dp_average_displacement" },
+      { name: "dp_core_area" },
+      { name: "dp_cpu" },
+      { name: "dp_delta_HPWL" },
+      { name: "dp_design_utilization" },
+      { name: "dp_legalized_HPWL" },
+      { name: "dp_max_displacement" },
+      { name: "dp_mem" },
+      { name: "dp_original_HPWL" },
+      { name: "dp_time" },
+      { name: "dp_total_cells" },
+      { name: "dp_total_displacement" },
+      { name: "droute_cpu" },
+      { name: "droute_defvias" },
+      { name: "droute_errors" },
+      { name: "droute_mem" },
+      { name: "droute_num_components" },
+      { name: "droute_num_instances" },
+      { name: "droute_num_layers" },
+      { name: "droute_num_macros" },
+      { name: "droute_num_nets" },
+      { name: "droute_num_terminals" },
+      { name: "droute_num_unique_instances" },
+      { name: "droute_num_vias" },
+      { name: "droute_peak_mem" },
+      { name: "droute_runtime" },
+      { name: "droute_time" },
+      { name: "droute_total_num_vias" },
+      { name: "droute_trackPts" },
+      { name: "droute_viols" },
+      { name: "droute_warnings" },
+      { name: "droute_wirelength" },
+      { name: "fastroute_cpu" },
+      { name: "fastroute_mem" },
+      { name: "fastroute_time" },
+      { name: "finish_area" },
+      { name: "finish_internal_power" },
+      { name: "finish_leakage_power" },
+      { name: "finish_switching_power" },
+      { name: "finish_total_power" },
+      { name: "finish_util" },
+      { name: "floorplan_area" },
+      { name: "floorplan_cpu" },
+      { name: "floorplan_io_count" },
+      { name: "floorplan_io_cpu" },
+      { name: "floorplan_io_mem" },
+      { name: "floorplan_io_time" },
+      { name: "floorplan_mem" },
+      { name: "floorplan_tdms_cpu" },
+      { name: "floorplan_tdms_mem" },
+      { name: "floorplan_tdms_time" },
+      { name: "floorplan_time" },
+      { name: "floorplan_tns" },
+      { name: "floorplan_util" },
+      { name: "floorplan_warnings" },
+      { name: "floorplan_wns" },
+      { name: "generate_date" },
+      { name: "globalplace_cpu" },
+      { name: "globalplace_hpwl" },
+      { name: "globalplace_mem" },
+      { name: "globalplace_time" },
+      { name: "globalplace_tns" },
+      { name: "globalplace_util" },
+      { name: "globalplace_ws" },
+      { name: "klayout_viols" },
+      { name: "merge_cpu" },
+      { name: "merge_mem" },
+      { name: "merge_time" },
+      { name: "mplace_cpu" },
+      { name: "mplace_macro_count" },
+      { name: "mplace_mem" },
+      { name: "mplace_solutions" },
+      { name: "mplace_time" },
+      { name: "openroad_commit" },
+      { name: "openroad_version" },
+      { name: "pdn_cpu" },
+      { name: "pdn_mem" },
+      { name: "pdn_time" },
+      { name: "platform" },
+      { name: "report_cpu" },
+      { name: "report_mem" },
+      { name: "report_time" },
+      { name: "resizer_cpu" },
+      { name: "resizer_hbuf_count" },
+      { name: "resizer_ibuf_count" },
+      { name: "resizer_maxcap_viols" },
+      { name: "resizer_maxfanout_bufs" },
+      { name: "resizer_maxfanout_viols" },
+      { name: "resizer_maxslew_viols" },
+      { name: "resizer_mem" },
+      { name: "resizer_obuf_count" },
+      { name: "resizer_post_area" },
+      { name: "resizer_post_tns" },
+      { name: "resizer_post_util" },
+      { name: "resizer_post_wns" },
+      { name: "resizer_pre_area" },
+      { name: "resizer_pre_tns" },
+      { name: "resizer_pre_util" },
+      { name: "resizer_pre_wns" },
+      { name: "resizer_resize_count" },
+      { name: "resizer_time" },
+      { name: "synth_cpu" },
+      { name: "synth_mem" },
+      { name: "synth_time" },
+      { name: "tapcell_cpu" },
+      { name: "tapcell_mem" },
+      { name: "tapcell_time" },
+      { name: "total_time" },
+      { name: "uuid" },
+      { name: "yosys_cell_count" },
+      { name: "yosys_chip_area" },
+      { name: "yosys_mem" },
+      { name: "yosys_runtime" },
+      { name: "yosys_version" },
+      { name: "yosys_warnings" }
+    ]
+  })
 };
 </script>
 
