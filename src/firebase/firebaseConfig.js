817e8a09b9bf392ae051bccff1a8a7e98ea78a32
Abdelrahman <abdelrahman@brown.edu>
2020-06-23 19:11:02 -0400

    update keys and change prod deployment

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,14 +6,14 @@ import firebase from 'firebase/app'
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyDnF5aKyrnDADK1bekKOca17W-yBPC1XcE",
-  authDomain: "openroad-dashboard.firebaseapp.com",
-  databaseURL: "https://openroad-dashboard.firebaseio.com",
-  projectId: "openroad-dashboard",
-  storageBucket: "openroad-dashboard.appspot.com",
-  messagingSenderId: "750287560029",
-  appId: "1:750287560029:web:feff8a7600dd1ea10c9cb4",
-  measurementId: "G-8QNZP7F5CW"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 
 firebase.initializeApp(config)
