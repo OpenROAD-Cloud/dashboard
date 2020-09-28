ff445cf36f78bf6b450825f617b91d82e2f9d2ef
Vitor Bandeira <vitor.vbandeira@gmail.com>
2020-09-28 15:41:18 -0300

    Reorg views

--- src/router.js
+++ src/router.js
@@ -14,76 +14,71 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const router = new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    scrollBehavior () {
-        return { x: 0, y: 0 }
-    },
-    routes: [
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior () {
+    return { x: 0, y: 0 }
+  },
+  routes: [
 
-        {
-    // =============================================================================
-    // MAIN LAYOUT ROUTES
-    // =============================================================================
-            path: '',
-            component: () => import('./layouts/main/Main.vue'),
-            children: [
+    {
+      // =============================================================================
+      // MAIN LAYOUT ROUTES
+      // =============================================================================
+      path: '',
+      component: () => import('./layouts/main/Main.vue'),
+      children: [
         // =============================================================================
         // Theme Routes
         // =============================================================================
-              {
-                path: '/',
-                name: 'home',
-                component: () => import('./views/Home.vue')
-              },
-              {
-                path: '/tsmc65lp-openroad',
-                name: 'tsmc65lp-openroad',
-                component: () => import('./views/TSMC65LP.vue')
-              },
-              {
-                path: '/gf14-openroad',
-                name: 'gf14-openroad',
-                component: () => import('./views/GF14.vue')
-              }
-            ],
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('./views/Home.vue')
         },
+        {
+          path: '/nangate45-openroad',
+          name: 'nangate45-openroad',
+          component: () => import('./views/pdks/nangate45.vue')
+        }
+      ],
+    },
     // =============================================================================
     // FULL PAGE LAYOUTS
     // =============================================================================
-        {
-            path: '',
-            component: () => import('@/layouts/full-page/FullPage.vue'),
-            children: [
+    {
+      path: '',
+      component: () => import('@/layouts/full-page/FullPage.vue'),
+      children: [
         // =============================================================================
         // PAGES
         // =============================================================================
-              {
-                path: '/pages/login',
-                name: 'page-login',
-                component: () => import('@/views/pages/Login.vue')
-              },
-              {
-                path: '/pages/error-404',
-                name: 'page-error-404',
-                component: () => import('@/views/pages/Error404.vue')
-              },
-            ]
-        },
-        // Redirect to 404 page, if no match found
         {
-            path: '*',
-            redirect: '/pages/error-404'
-        }
-    ],
+          path: '/pages/login',
+          name: 'page-login',
+          component: () => import('@/views/Login.vue')
+        },
+        {
+          path: '/pages/error-404',
+          name: 'page-error-404',
+          component: () => import('@/views/Error404.vue')
+        },
+      ]
+    },
+    // Redirect to 404 page, if no match found
+    {
+      path: '*',
+      redirect: '/pages/error-404'
+    }
+  ],
 })
 
 router.afterEach(() => {
   // Remove initial loading
   const appLoading = document.getElementById('loading-bg')
-    if (appLoading) {
-        appLoading.style.display = "none";
-    }
+  if (appLoading) {
+    appLoading.style.display = "none";
+  }
 })
 
 export default router
--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -3,7 +3,7 @@
     Description: 404 Page
     ----------------------------------------------------------------------------------------
     Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-      Author: Pixinvent
+    Author: Pixinvent
     Author URL: http://www.themeforest.net/user/pixinvent
 ========================================================================================== -->
 
