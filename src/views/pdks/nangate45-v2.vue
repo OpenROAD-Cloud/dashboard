0add5caa7c2da8f31adf83d06616470c9105180a
Vitor Bandeira <vitor.vbandeira@gmail.com>
2021-03-01 05:42:10 -0300

    Create base for db v2

---
+++ src/views/pdks/nangate45-v2.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Nangate45</h1>
+    <br />
+    <vs-tabs :color="colorx">
+      <vs-tab label="aes">          <Metrics designName="aes"         libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="dynamicnode">  <Metrics designName="dynamicnode" libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="gcd">          <Metrics designName="gcd"         libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="ibex">         <Metrics designName="ibex"        libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="jpeg">         <Metrics designName="jpeg"        libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="swerv">        <Metrics designName="swerv"       libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="tinyRocket">   <Metrics designName="tinyrocket"  libraryName="nangate45" :metrics=metrics></Metrics> </vs-tab>
+    </vs-tabs>
+  </div>
+</template>
+
+<script>
+
+import Metrics from '../../layouts/components/Metrics'
+import metadataSchema from "../schemas/metadata.schema-v2.json";
+
+export default {
+  created: function() {
+    this.loadSchema();
+  },
+  components: {
+    Metrics,
+  },
+  methods: {
+    loadSchema() {
+      let metrics = [];
+      for (var p in metadataSchema.properties) {
+        metrics.push({
+          name: p,
+          trend: metadataSchema.properties[p]["improvement"]
+        });
+      }
+      this.metrics = metrics;
+    }
+  },
+  data: () => ({
+    colorx: "dark",
+    metrics: []
+  })
+};
+</script>
---
+++ src/views/pdks/sky130hd-v2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Sky130hd</h1>
+    <br />
+    <vs-tabs :color="colorx">
+      <vs-tab label="aes">          <Metrics designName="aes"         libraryName="sky130hd" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="gcd">          <Metrics designName="gcd"         libraryName="sky130hd" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="ibex">         <Metrics designName="ibex"        libraryName="sky130hd" :metrics=metrics></Metrics> </vs-tab>
+      <vs-tab label="jpeg">         <Metrics designName="jpeg"        libraryName="sky130hd" :metrics=metrics></Metrics> </vs-tab>
+    </vs-tabs>
+  </div>
+</template>
+
+<script>
+
+import Metrics from '../../layouts/components/Metrics'
+import metadataSchema from "../schemas/metadata.schema-v2.json";
+
+export default {
+  created: function() {
+    this.loadSchema();
+  },
+  components: {
+    Metrics,
+  },
+  methods: {
+    loadSchema() {
+      let metrics = [];
+      for (var p in metadataSchema.properties) {
+        metrics.push({
+          name: p,
+          trend: metadataSchema.properties[p]["improvement"]
+        });
+      }
+      this.metrics = metrics;
+    }
+  },
+  data: () => ({
+    colorx: "dark",
+    metrics: []
+  })
+};
+</script>
